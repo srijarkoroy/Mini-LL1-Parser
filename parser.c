d957bd4a5b2d64c94f1bd87095e8f00b31258a45
Rahul Anand <57794377+CrAzYJOKR@users.noreply.github.com>
2022-04-24 15:39:58 +0530

    feat: added the findfirst functionality

--- parser.c
+++ parser.c
@@ -292,3 +292,156 @@ int main(int argc,char **argv)
 	char stack[100];
 	stack[0] = '$';
 	stack[1] = table[0][0];
+	while(s_ptr != -1){
+		printf("\t\t\t\t\t\t");
+		int vamp = 0;
+		for(vamp=0;vamp<=s_ptr;vamp++){
+			printf("%c",stack[vamp]);
+		}
+		printf("\t\t\t");
+		vamp = i_ptr;
+		while(input[vamp] != '\0'){
+			printf("%c",input[vamp]);
+			vamp++;
+		}
+		printf("\t\t\t");
+		char her = input[i_ptr];
+		char him = stack[s_ptr];
+		s_ptr--;
+		if(!isupper(him)){
+			if(her == him){
+				i_ptr++;
+				printf("POP ACTION\n");
+			}
+			else{
+				printf("\nString Not Accepted by LL(1) Parser !!\n");
+				exit(0);
+			}
+		}
+		else{
+			for(i=0;i<sid;i++){
+				if(ter[i] == her)
+					break;
+			}
+			char produ[100];
+			for(j=0;j<land;j++){
+				if(him == table[j][0]){
+					if (table[j][i+1] == '#'){
+						printf("%c=#\n",table[j][0]);
+						produ[0] = '#';
+						produ[1] = '\0';
+					}
+					else if(table[j][i+1] != '!'){
+						int mum = (int)(table[j][i+1]);
+						mum -= 65;
+						strcpy(produ,production[mum]);
+						printf("%s\n",produ);
+					}
+					else{
+						printf("\nString Not Accepted by LL(1) Parser !!\n");
+						exit(0);
+					}
+				}
+			}
+			int le = strlen(produ);
+			le = le - 1;
+			if(le == 0){
+				continue;
+			}
+			for(j=le;j>=2;j--){
+				s_ptr++;
+				stack[s_ptr] = produ[j];
+			}
+		}
+	}
+	printf("\n\t\t\t=======================================================================================================================\n");
+	if (input[i_ptr] == '\0'){
+		printf("\t\t\t\t\t\t\t\tYOUR STRING HAS BEEN ACCEPTED !!\n");
+	}
+	else
+		printf("\n\t\t\t\t\t\t\t\tYOUR STRING HAS BEEN REJECTED !!\n");
+	printf("\t\t\t=======================================================================================================================\n");
+}
+
+void follow(char c)
+{
+	int i ,j;
+	if(production[0][0]==c){
+ 		f[m++]='$';
+ 	}
+ 	for(i=0;i<10;i++)
+ 	{
+  		for(j=2;j<10;j++)
+  		{
+   			if(production[i][j]==c)
+   			{
+    			if(production[i][j+1]!='\0'){
+					followfirst(production[i][j+1],i,(j+2));
+ 				}
+    			if(production[i][j+1]=='\0'&&c!=production[i][0]){
+     				follow(production[i][0]);
+				}
+   			}   
+  		}
+ 	}
+}
+
+void findfirst(char c ,int q1 , int q2)
+{
+	int j;
+	if(!(isupper(c))){
+		first[n++]=c;
+	}
+	for(j=0;j<count;j++)
+	{
+		if(production[j][0]==c)
+		{
+			if(production[j][2]=='#'){
+				if(production[q1][q2] == '\0')
+					first[n++]='#';
+				else if(production[q1][q2] != '\0' && (q1 != 0 || q2 != 0))
+				{
+					findfirst(production[q1][q2], q1, (q2+1));
+				}
+				else
+					first[n++]='#';
+			}
+			else if(!isupper(production[j][2])){
+				first[n++]=production[j][2];
+			}
+			else {
+				findfirst(production[j][2], j, 3);
+			}
+		}
+	}	
+}
+
+void followfirst(char c, int c1 , int c2)
+{
+    int k;
+    if(!(isupper(c)))
+		f[m++]=c;
+	else{
+		int i=0,j=1;
+		for(i=0;i<count;i++)
+		{
+			if(calc_first[i][0] == c)
+				break;
+		}
+		while(calc_first[i][j] != '!')
+		{
+			if(calc_first[i][j] != '#'){
+				f[m++] = calc_first[i][j];
+			}
+			else{
+				if(production[c1][c2] == '\0'){
+					follow(production[c1][0]);
+				}
+				else{
+					followfirst(production[c1][c2],c1,c2+1);
+				}
+			}
+			j++;
+		}
+	}
+}
